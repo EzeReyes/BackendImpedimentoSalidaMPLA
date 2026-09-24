e10bfbfe6df214f7b0c7d10294cce3b0f509ae6b
Ezequiel Reyes <ese.reyes1992@gmail.com>
2026-09-24 10:08:37 -0300

    working in login

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
-const app = express();
-
-// Middlewares
-app.use(cookieParser());
-
 app.use(
   cors({
     origin: "https://buquesimpedimentompla.vercel.app",
     // origin: "http://localhost:5173",
-    credentials: false,
+    credentials: true,
   })
 );
+
+const app = express();
+
+// Middlewares
+app.use(cookieParser());
 
 app.use(express.json());
 
