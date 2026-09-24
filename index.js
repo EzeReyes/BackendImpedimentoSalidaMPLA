e81837d4a405e8768884761e3f55252f6ed1ccc0
Ezequiel Reyes <ese.reyes1992@gmail.com>
2026-09-24 09:41:55 -0300

    working in login

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://buquesimpedimentompla.vercel.app/"
+    // origin: "http://localhost:5173",
     credentials: true,
   })
 );
